fa0506154c1ab39f60e49dad67fc66cb49fcd658
Trouble-Andrew <satyr663@gmail.com>
2022-04-18 14:36:25 +0300

    add: 18 task

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -1,7 +1,5 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-// console.log(bands);
-
 function strip(bandName) {
     return bandName.replace(/^(a |the |an )/i, '').trim();
 }
@@ -9,6 +7,3 @@ function strip(bandName) {
 const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 
 document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
-
-console.log(sortedBands);
-  
